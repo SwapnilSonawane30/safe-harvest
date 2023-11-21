d3c1f098decefa98a0b37dc89bbc577b58399858
Sonawane Swapnil Chandrakant <104548694+SwapnilSonawane30@users.noreply.github.com>
2023-11-21 22:14:22 +0530

    final

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -6,7 +6,7 @@ import {
   MemberRole,
   Server
 } from "@prisma/client";
-import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
+import { Edit, Hash, Lock, Mic, Trash, Video,Leaf } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
@@ -20,7 +20,7 @@ interface ServerChannelProps {
 }
 
 const iconMap = {
-  [ChannelType.TEXT]: Hash,
+  [ChannelType.TEXT]: Leaf,
   [ChannelType.AUDIO]: Mic,
   [ChannelType.VIDEO]: Video,
 }
